df9c9e097c98356198334ad3ab4309b5151f14f7
Eric Sartorius <ericssartorius@gmail.com>
2024-09-01 08:44:44 +0100

    Update constants

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -1,16 +1,16 @@
 import { dev } from '$app/environment';
-export const BASE_URL = dev ? 'http://localhost:5173' : 'https://sveltekit-auth-alpha.vercel.app';
-export const APP_NAME = 'Sveltekit Auth';
-export const CONTACT_EMAIL = 'yourname@email.com';
-export const DOMAIN = 'sveltekit-auth-alpha.vercel.app';
+export const BASE_URL = dev ? 'http://localhost:5173' : 'https://purefunc.io';
+export const APP_NAME = 'Pure Func';
+export const CONTACT_EMAIL = 'contact@purefunc.io';
+export const DOMAIN = 'purefunc.io';
 /* WARNING!!! TERMS AND CONDITIONS AND PRIVACY POLICY 
 WERE CREATED BY CHATGPT AS AN EXAMPLE ONLY. 
 CONSULT A LAWYER AND DEVELOP YOUR OWN TERMS AND PRIVACY POLICY!!! */
-export const TERMS_PRIVACY_CONTACT_EMAIL = 'yourname@email.com';
-export const TERMS_PRIVACY_WEBSITE = 'yourdomain.com';
-export const TERMS_PRIVACY_COMPANY = 'Your Company';
-export const TERMS_PRIVACY_EFFECTIVE_DATE = 'January 1, 2023';
-export const TERMS_PRIVACY_APP_NAME = 'Your App';
+export const TERMS_PRIVACY_CONTACT_EMAIL = 'contact@purefunc.io';
+export const TERMS_PRIVACY_WEBSITE = 'purefunc.io';
+export const TERMS_PRIVACY_COMPANY = 'Pure Func';
+export const TERMS_PRIVACY_EFFECTIVE_DATE = 'January 1, 2024';
+export const TERMS_PRIVACY_APP_NAME = 'Pure Func';
 export const TERMS_PRIVACY_APP_PRICING_AND_SUBSCRIPTIONS =
 	'[Details about the pricing, subscription model, refund policy]';
 export const TERMS_PRIVACY_COUNTRY = 'United States';
